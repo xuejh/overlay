3bfaeb780a0a14a692a1cf2aeec57c220af4049c
薛锦辉 <327646128@qq.com>
2018-08-01 15:32:10 +0800

    完善代码

--- AccessibilityOverlayDemo/AccessibilityOverlayDemo/DrawView.m
+++ AccessibilityOverlayDemo/AccessibilityOverlayDemo/DrawView.m
@@ -17,6 +17,15 @@
 
 @implementation DrawView
 
+- (instancetype) init{
+    
+    self = [super init];
+    if (self) {
+        self.isAccessibilityElement = YES;
+        self.accessibilityLabel = @"程序员";
+    }
+    return self;
+}
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -29,48 +38,48 @@
 }
 
 
--(BOOL)isAccessibilityElement
-{
-    return NO;
-}
-
--(NSArray*)getAccessElement
-{
-    if(_AccessElements)
-    {
-        return _AccessElements;
-    }
-    
-    _AccessElements = [[NSMutableArray alloc] init];
-    
-    UIAccessibilityElement *element = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self] ;
-    element.isAccessibilityElement = YES;
-    element.accessibilityTraits = UIAccessibilityTraitNone;
-    
-    CGRect frame = CGRectMake(0, 0, 100, 50);
-    element.accessibilityFrame = [self convertRect:frame toView:nil];// 转换为屏幕坐标
-    element.accessibilityLabel = @"程序员";
-    [_AccessElements addObject:element];
-    
-    
-    
-    
-    return _AccessElements;
-}
-
-- (NSInteger)accessibilityElementCount
-{
-    return [self getAccessElement].count;
-}
-
-- (id)accessibilityElementAtIndex:(NSInteger)index
-{
-    return [[self getAccessElement] objectAtIndex:index];
-}
-
-- (NSInteger)indexOfAccessibilityElement:(id)element
-{
-    return [[self getAccessElement] indexOfObject:element];
-}
+//-(BOOL)isAccessibilityElement
+//{
+//    return NO;
+//}
+//
+//-(NSArray*)getAccessElement
+//{
+//    if(_AccessElements)
+//    {
+//        return _AccessElements;
+//    }
+//
+//    _AccessElements = [[NSMutableArray alloc] init];
+//
+//    UIAccessibilityElement *element = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self] ;
+//    element.isAccessibilityElement = YES;
+//    element.accessibilityTraits = UIAccessibilityTraitNone;
+//
+//    CGRect frame = CGRectMake(0, 0, 100, 50);
+//    element.accessibilityFrame = [self convertRect:frame toView:nil];// 转换为屏幕坐标
+//    element.accessibilityLabel = @"程序员";
+//    [_AccessElements addObject:element];
+//
+//
+//
+//
+//    return _AccessElements;
+//}
+//
+//- (NSInteger)accessibilityElementCount
+//{
+//    return [self getAccessElement].count;
+//}
+//
+//- (id)accessibilityElementAtIndex:(NSInteger)index
+//{
+//    return [[self getAccessElement] objectAtIndex:index];
+//}
+//
+//- (NSInteger)indexOfAccessibilityElement:(id)element
+//{
+//    return [[self getAccessElement] indexOfObject:element];
+//}
 
 @end
